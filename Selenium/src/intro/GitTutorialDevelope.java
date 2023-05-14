bd48dbd045820f9a558c87f2b664306c405856c9
vinoth144 <dnshbabu25@gmail.com>
2023-05-14 18:16:20 +0530

    tutorial develope guy commit

---
+++ Selenium/src/intro/GitTutorialDevelope.java
@@ -0,0 +1,14 @@
+package intro;
+
+public class GitTutorialDevelope {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("develope tutorial");
+		System.out.println("develope yourself");
+		
+		
+	}
+
+}
